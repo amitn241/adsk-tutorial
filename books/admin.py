498c09424f36cf1039da6a363c67d9643792f53c
Amit Nabarro <amit.nabarro@gmail.com>
2014-12-04 08:59:46 +0200

    added an inline admin for the Author through model in Book Admin

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+
+class AuthorInline(admin.TabularInline):
+    model = Author
+    extra = 0 
+
 
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('id','title','published_date') 
+	inlines = (AuthorInline,)
 
 
 admin.site.register(Book, BookAdmin)
